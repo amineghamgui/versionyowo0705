a5df97da05d47e2be9bbca5b0dfc922904bfbbd2
amineghamgui <125130818+amineghamgui@users.noreply.github.com>
2024-05-07 18:37:50 +0100

    Update train_trasfert_learning_ava.py

--- train_trasfert_learning_ava.py
+++ train_trasfert_learning_ava.py
@@ -361,7 +361,7 @@ def train():
                     model_eval.trainable = True
                     h=evaluator.loss_validation(model_eval, epoch + 1)
                     wandb.log({"frame_map": evaluator.evaluate_frame_map(model_eval, epoch + 1),
-                              "loss_conf train":a,"loss_cls train":b,"loss_box train ":c,"losses train ":d, "loss validation"h })
+                              "loss_conf train":a,"loss_cls train":b,"loss_box train ":c,"losses train ":d, "loss validation":h })
                     model_eval.train()
         
                 # save model
